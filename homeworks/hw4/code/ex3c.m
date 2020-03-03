e39dc662cd1166711995dc7cd635b49fc73c3ba8
Yves Greatti <yvgrotti@gmail.com>
2020-03-03 18:44:06 -0500

    Hw4 for now

--- homeworks/hw4/code/ex3c.m
+++ homeworks/hw4/code/ex3c.m
@@ -27,7 +27,7 @@ disp(partition')
 function [X, cut_value] = maxcut(n, W)
 cvx_begin sdp
     variable X(n,n) symmetric
-    maximize 0.5 .* sum(sum(W .* (ones(n,n) - X))) 
+    maximize 0.25 .* sum(sum(W .* (ones(n,n) - X))) 
     diag(X) == 1;
     X >= 0;
 cvx_end
@@ -41,7 +41,7 @@ r = (1/sqrt(n)) .* ones(n, 1);
 
 for i=1:n
     if dot(V(i,:), r) >= 0
-        partition(i) = 1;
+        partition(i) = i;
     else
         partition(i) = 0;
     end
